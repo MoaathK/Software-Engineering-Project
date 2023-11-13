f66444c7462ce80b5e8385afe3331c62940b71d7
Moath <109114154+MoaathK@users.noreply.github.com>
2023-11-13 14:52:50 +0300

    new Update

--- demo/src/main/java/module-info.java
+++ demo/src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module com.example.demo {
+    requires javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
-    opens com.example.demo to javafx.fxml;
+
+    opens com.example.demo; //to javafx.fxml;
     exports com.example.demo;
 }
